2dda0d93272ab06cb91da1f39f7342452056f1fb
HL <harman.loodu@gmail.com>
2025-08-26 17:25:30 -0400

    Fix ntlk?

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import seaborn as sns
 from wordcloud import WordCloud
 
 # Set custom nltk_data path
-nltk_data_path = os.path.join(os.getcwd(), 'resources', 'nltk_data')
-nltk.data.path.append(nltk_data_path)
+nltk_data_path = os.path.join(os.path.dirname(__file__), 'resources', 'nltk_data')
+nltk.data.path.insert(0, nltk_data_path)
 
 
 # get dataset for visuals
